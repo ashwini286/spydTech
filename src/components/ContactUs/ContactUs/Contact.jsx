ee3e2d69075871b293235f0f22b6717097267c75
unknown <ashwini.swe@gmail.com>
2024-01-25 22:18:20 +0530

    contact page updated

--- src/components/ContactUs/ContactUs/Contact.jsx
+++ src/components/ContactUs/ContactUs/Contact.jsx
@@ -3,10 +3,10 @@ import Navbarupp from '../../Home/Navbar/Navbarupp'
 const Contact = () => {
   return (
     <>
-    <div className="bg-black border border-black">
+    <div>
     <Navbarupp />
     </div>
-    <div>
+    <div className='mt-16'>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d121797.7749059575!2d78.31039743892126!3d17.45107444800503!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb9107fdd2049d%3A0xc828116f8f70b747!2sVisa%20Global%20365!5e0!3m2!1sen!2sin!4v1706198889906!5m2!1sen!2sin"
         width="100%"
