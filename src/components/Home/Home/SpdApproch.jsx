bba190068c7da7b76dcd38dbbf2e8e013d894a3e
Prabhashyam <116556508+7893787849@users.noreply.github.com>
2024-02-06 20:51:03 -0800

    hi

--- src/components/Home/Home/SpdApproch.jsx
+++ src/components/Home/Home/SpdApproch.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import './Spdapproch.css'; // Import CSS file for styling
 import { FaHandshake } from "react-icons/fa6";
+import { FaGlobe } from "react-icons/fa";
+import { IoIosLeaf } from "react-icons/io";
+import { MdGroups } from "react-icons/md";
 
 function SpdApproch() {
   return (
     <>
-    <div className='flex ml-10 space-x-10'>
+    <div className='flex ml-10 space-x-2'>
     <div className="w-64 h-40 relative">
       <div className="group w-full h-full perspective">
         <div className="flip-card">
           <div className="flip-card-inner">
-            <div className="flip-card-front">
+            <div className="flip-card-front bg-[#0891b2]">
               <a href="#" className="card-link">
-                <FaHandshake className='text-8xl ml-14'/>
-                <p className="font-normal text-gray-700">This is the front side of the card.</p>
+                <FaHandshake className='text-6xl ml-14 text-white'/>
+                <p className="font-bold text-white text-center">High added Value Services</p>
               </a>
             </div>
-            <div className="flip-card-back">
+            <div className="flip-card-back bg-[#0891b2]">
               <a href="#" className="card-link">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Back Content</h5>
-                <p className="font-normal text-gray-700">This is the back side of the card.</p>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Back Content</h5>
+                <p className="font-normal text-white">This is the back side of the card.</p>
               </a>
             </div>
           </div>
@@ -28,20 +31,20 @@ function SpdApproch() {
     </div>
 
     {/* 2nd card */}
-    <div className="w-64 h-40 relative">
-      <div className="group w-full h-full perspective">
-        <div className="flip-card">
+    <div className="w-64 h-40 relative ">
+      <div className="group w-full h-full perspective ">
+        <div className="flip-card ">
           <div className="flip-card-inner">
-            <div className="flip-card-front">
+            <div className="flip-card-front bg-[#0e7490] ">
               <a href="#" className="card-link">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Front Content</h5>
-                <p className="font-normal text-gray-700">This is the front side of the card.</p>
+                <FaGlobe className='text-6xl ml-14 text-white'/>
+                <p className="text-white text-center font-bold">Globel approch</p>
               </a>
             </div>
-            <div className="flip-card-back">
+            <div className="flip-card-back bg-[#0e7490]">
               <a href="#" className="card-link">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Back Content</h5>
-                <p className="font-normal text-gray-700">This is the back side of the card.</p>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Back Content</h5>
+                <p className="font-normal text-white">This is the back side of the card.</p>
               </a>
             </div>
           </div>
@@ -53,16 +56,16 @@ function SpdApproch() {
       <div className="group w-full h-full perspective">
         <div className="flip-card">
           <div className="flip-card-inner">
-            <div className="flip-card-front">
+            <div className="flip-card-front bg-[#14b8a6]">
               <a href="#" className="card-link">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Front Content</h5>
-                <p className="font-normal text-gray-700">This is the front side of the card.</p>
+              <MdGroups className='text-6xl ml-14 text-white'/>
+                <p className="text-white text-center font-bold">Human and talents at the</p>
               </a>
             </div>
-            <div className="flip-card-back">
+            <div className="flip-card-back bg-[#14b8a6]">
               <a href="#" className="card-link">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Back Content</h5>
-                <p className="font-normal text-gray-700">This is the back side of the card.</p>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Back Content</h5>
+                <p className="font-normal text-white">This is the back side of the card.</p>
               </a>
             </div>
           </div>
@@ -74,16 +77,16 @@ function SpdApproch() {
       <div className="group w-full h-full perspective">
         <div className="flip-card">
           <div className="flip-card-inner">
-            <div className="flip-card-front">
+            <div className="flip-card-front bg-[#60a5fa]">
               <a href="#" className="card-link">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Front Content</h5>
-                <p className="font-normal text-gray-700">This is the front side of the card.</p>
+                <IoIosLeaf className='text-6xl ml-14 text-white'/>
+                <p className="text-white text-center font-bold">This is the front side of the card.</p>
               </a>
             </div>
-            <div className="flip-card-back">
+            <div className="flip-card-back bg-[#60a5fa]">
               <a href="#" className="card-link">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Back Content</h5>
-                <p className="font-normal text-gray-700">This is the back side of the card.</p>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Back Content</h5>
+                <p className="font-normal text-white">This is the back side of the card.</p>
               </a>
             </div>
           </div>
