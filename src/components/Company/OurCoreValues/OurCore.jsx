ece782dbc561aa228686aeba47fa60dd205e74db
unknown <ashwini.swe@gmail.com>
2024-02-15 12:54:00 +0530

    working on OurCore page

--- src/components/Company/OurCoreValues/OurCore.jsx
+++ src/components/Company/OurCoreValues/OurCore.jsx
@@ -7,6 +7,11 @@ const OurCore = () => {
     <div>
       <Navbarupp />
       </div>   
+      <img
+        src="https://www.teleioscn.org/hubfs/corevalue_main.jpg"
+        className=" object-cover  w-full"
+        alt="Background Image"
+      />
     </>
   )
 }
