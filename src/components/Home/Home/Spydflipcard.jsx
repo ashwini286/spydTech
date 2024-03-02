21b1e0e0cd1ac25a48008e0565f9591cd021d31c
Prabhashyam <116556508+7893787849@users.noreply.github.com>
2024-03-02 23:32:46 +0530

    spydapproch, smm page

--- src/components/Home/Home/Spydflipcard.jsx
+++ src/components/Home/Home/Spydflipcard.jsx
@@ -11,72 +11,126 @@ function Spydflipcard() {
     {
       icon: FaHandshake,
       title: "High added Value Services",
+      frontContent: "Front content 1",
       backContent: "Back content 1",
     },
     {
       icon: FaGlobe,
-      title: "High added Value Services",
+      title: "Global Approach",
+      frontContent: "Front content 2",
       backContent: "Back content 2",
     },
     {
       icon: IoIosLeaf,
-      title: "High added Value Services",
+      title: "Commitment to a Virtuous Digital Transition",
+      frontContent: "Front content 3",
       backContent: "Back content 3",
     },
     {
       icon: MdGroups,
-      title: "High added Value Services",
+      title: "Human and Talents at the Heart of our Development",
+      frontContent: "Front content 4",
       backContent: "Back content 4",
     },
   ];
 
   return (
-    <>
-      <div className="et_pb_row et_pb_row_2 et_pb_row_4col">
-        {FlipBoxData.map((flipBox, index) => (
-          <div key={index} className="et_pb_column et_pb_column_1_4 et_pb_column_3  et_pb_css_mix_blend_mode_passthrough">
-            <div className="et_pb_module ba_flipbox ba_flipbox_0">
-              <div className="et_pb_module_inner">
-                <div className="dtq-module dtq-flipbox dtq-flipbox--flip dtq-flip-right">
-                  <div className="dtq-flipbox-inner">
-                    <div className="dtq-flipbox-card-container">
-                      <div className="dtq-flipbox-front-card dtq-flipbox-card">
-                        <div className="dtq-flipbox-card-inner">
-                          <div className="dtq-flipbox-front-content dtq-flipbox-content">
-                            <div className="dtq-flipbox-figure-front">
-                              <div className="dtq-flipbox-icon dtq-flipbox-icon-front">
-                                <flipBox.icon />
-                              </div>
+    <div className="et_pb_row et_pb_row_2 et_pb_row_4col">
+      {FlipBoxData.map((flipBox, index) => (
+        <div key={index} className="et_pb_column et_pb_column_1_4 et_pb_column_3  et_pb_css_mix_blend_mode_passthrough">
+          <div className="et_pb_module ba_flipbox ba_flipbox_0">
+            <div className="et_pb_module_inner">
+              <div className="dtq-module dtq-flipbox dtq-flipbox--flip dtq-flip-right">
+                <div className="dtq-flipbox-inner">
+                  <div className="dtq-flipbox-card-container">
+                    <div className="dtq-flipbox-front-card dtq-flipbox-card">
+                      <div className="dtq-flipbox-card-inner">
+                        <div className="dtq-flipbox-front-content dtq-flipbox-content">
+                          <div className="dtq-flipbox-figure-front">
+                            <div className="dtq-flipbox-icon dtq-flipbox-icon-front">
+                              <flipBox.icon />
                             </div>
-                            <div className="dtq-flipbox-content-wrap">
-                              <h2 className="dtq-flipbox-title-front">{flipBox.title}</h2>
-                              <div className="dtq-flipbox-content">{flipBox.frontContent}</div>
-                            </div>
+                          </div>
+                          <div className="dtq-flipbox-content-wrap">
+                            <h2 className="dtq-flipbox-title-front">{flipBox.title}</h2>
+                            <div className="dtq-flipbox-content">{flipBox.frontContent}</div>
                           </div>
                         </div>
                       </div>
-                      <div className="dtq-flipbox-back-card dtq-flipbox-card">
-                        <div className="dtq-flipbox-card-inner">
-                          <div className="dtq-flipbox-back-content dtq-flipbox-content">
-                            <div className="dtq-flipbox-figure-back">
-                              <div className="dtq-flipbox-img-back">
-                                <img decoding="async" src={IMG} alt="" />
-                              </div>
-                            </div>
-                            <div className="dtq-flipbox-content-wrap">{flipBox.backContent}</div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="dtq-flank"></div>
                     </div>
+                    <div className="dtq-flipbox-back-card dtq-flipbox-card">
+                      <div className="dtq-flipbox-card-inner">
+                        <div className="dtq-flipbox-back-content dtq-flipbox-content">
+                          <div className="dtq-flipbox-figure-back">
+                            <div className="dtq-flipbox-img-back">
+                              <img decoding="async" src={IMG} alt="" />
+                            </div>
+                          </div>
+                          <div className="dtq-flipbox-content-wrap">{flipBox.backContent}</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="dtq-flank"></div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        ))}
-      </div>
-    </>
+        </div>
+      ))}
+      <style>
+        {`
+         .flip-card {
+          width: 80%;
+          height: 90%;
+          
+          position: relative;
+          transform-style: preserve-3d;
+          transition: transform 0.6s;
+        }
+        
+        .flip-card:hover {
+          transform: rotateY(180deg);
+        }
+        
+        .flip-card-inner {
+          width: 100%;
+          height: 150px;
+          position: absolute;
+          transform-style: preserve-3d;
+        }
+        
+        .flip-card-front,
+        .flip-card-back {
+          width: 100%;
+          height: 100%;
+          position: absolute;
+          backface-visibility: hidden;
+          border-radius: 12%;
+        }
+        
+       
+        
+        .flip-card-back {
+          /* background-color: #fff; */
+          transform: rotateY(180deg);
+        }
+        
+        .card-link {
+          display: block;
+          width: 100%;
+          height: 100%;
+          padding: 1rem;
+          border: 1px solid white;
+           border-radius: 0.25rem; 
+          text-decoration: none;
+          color: inherit;
+        }
+        
+        `}
+        </style>
+    </div>
+    
   );
 }
 
---
+++ src/components/Services/DigitalMarketing/Media.jsx
@@ -0,0 +1,121 @@
+import React from 'react'
+
+function Media() {
+  return (
+    <>
+    <section class="overflow-hidden bg-white py-8 sm:py-16">
+        <div className='p-10 text-4xl text-center'>
+        <h1>FACEBOOK<a className='p-2'>MARKETING:</a></h1>
+        </div>
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+      <div class="lg:pr-8 lg:pt-4">
+        <div class="lg:max-w-lg">
+        <h2 class="text-base font-semibold leading-7 "><a className='text-2xl text-blue-500'>Facebook</a> is one of the oldest and most profound social media channels. 
+        It is vital for any company to have a Facebook presence whereas it reflects the company’s identity and values. 
+        Facebook also offers detailed advertising targeting based on the various data points they have collected for each of their users, 
+        which implies great opportunities for your company.</h2>
+          
+          
+            </div>
+            <h2 class="text-xl font-semibold mb-2">DomainStar specifically offers the following services:</h2>
+            <ul class="list-disc list-inside mb-4">
+                <li class="mb-2">Facebook Business Page Design & Creation</li>
+                <li class="mb-2">Facebook Monthly Calendar Content Creation</li>
+                <li class="mb-2">Facebook Page Management</li>
+                <li class="mb-2">Facebook Page Monitoring</li>
+                <li class="mb-2">Facebook Targeted Marketing Campaigns</li>
+                <li class="mb-2">Facebook Competitions</li>
+                <li class="mb-2">Facebook Performance Reports</li>
+               
+            </ul>
+
+       
+      </div>
+      <img src="https://www.pixelstalk.net/wp-content/uploads/2016/06/Facebook-Background.jpg"
+       alt="Product screenshot" class=" w-[48rem] md:w-full h-full rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0" width="2432" height="1442" />
+    </div>
+  </div>
+</section>
+
+
+
+
+<div class="max-w-screen-xl mx-auto  ">
+    <div class="text-center mb-10">
+        <h2 class="text-4xl tracking-tight font-bold text-primary-800">INSTAGRAM<a className='text-4xl tracking-tight font-bold text-primary-800'>MARKETING:</a></h2>
+    </div>
+
+    <div class="flex flex-col md:flex-row">
+        
+        <div class="mr-0 md:mr-8 mb-6 md:mb-0">
+            <img class="w-[40-rem] md:w-full mx-auto rounded-xl h-full md:ml-[-10%]" src="https://cdndailyexcelsior.b-cdn.net/wp-content/uploads/2022/06/instagram.jpg" 
+            alt="can_help_banner" />
+        </div>
+        <div class="lg:pl- lg:pt-4">
+        <div class="lg:max-w-5xl">
+        <h2 class="text-base font-semibold leading-7 "><a className='text-2xl text-blue-500'>Instagram</a> is the ‘baby-brother’ of Facebook. Owned by the same company as Facebook, 
+        Instagram is like a simplified version of Facebook, with an emphasis on mobile use and visual sharing. As DreamGrow stated,
+         “The best part about Instagram is that it has some of the most highly engaged users”. Moreover, BrandWatch indicated that, 
+         “90 percent of Instagram users are younger than 35”. This makes Instagram an excellent B2C marketing tool for any business, 
+         small and big.</h2>
+          
+          
+            </div>
+            <h2 class="text-xl font-semibold mb-2">DomainStar offers the following services:</h2>
+            <ul class="list-disc list-inside mb-4">
+                <li class="mb-2">Instagram Business Page Design & Creation</li>
+                <li class="mb-2">Instagram Monthly Calendar Content Creation (for posts and stories)</li>
+                <li class="mb-2">Instagram Page Management</li>
+                <li class="mb-2">Instagram Page Monitoring</li>
+                <li class="mb-2">Instagram Targeted Marketing Campaigns</li>
+                <li class="mb-2">Instagram Competitions</li>
+                <li class="mb-2">Instagram Performance Reports</li>
+               
+            </ul>
+
+       
+      </div>
+       
+</div>
+</div>
+
+<section class="overflow-hidden bg-white py-8 sm:py-16">
+        <div className='p-10 text-4xl text-center'>
+        <h1>LINKEDIN<a className='p-2'>MARKETING:</a></h1>
+        </div>
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+      <div class="lg:pr-8 lg:pt-4">
+        <div class="lg:max-w-lg">
+        <h2 class="text-base font-semibold leading-7 "><a className='text-2xl text-blue-500'>LinkedIn</a> is the most powerful B2B 
+        (business-to-business) social media channel. It has been named the Facebook for Professionals.
+         LinkedIn allows you to connect with the company’s decision-maker while also enables recruitment 
+         via the social media network. LinkedIn is a very effective tool for generating new business leads and nurturing 
+         referral relationships.</h2>
+          
+          
+            </div>
+            <h2 class="text-xl font-semibold mb-2">DomainStar specifically offers the following services:</h2>
+            <ul class="list-disc list-inside mb-4">
+                <li class="mb-2">LinkedIn Business Page Creation & Optimisation</li>
+                <li class="mb-2">LinkedIn Monthly Calendar Content Creation</li>
+                <li class="mb-2">LinkedIn Targeted Advertising Campaigns</li>
+                <li class="mb-2">LinkedIn Page Management & Consulting</li>
+                <li class="mb-2">LinkedIn Performance Reports</li>
+               
+               
+            </ul>
+
+       
+      </div>
+      <img src="https://images.unsplash.com/photo-1611944212129-29977ae1398c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGlua2VkaW58ZW58MHx8MHx8fDA%3D"
+       alt="Product screenshot" class="w-[48rem]  rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0" width="2432" height="1442" />
+    </div>
+  </div>
+</section>
+    </>
+  )
+}
+
+export default Media
