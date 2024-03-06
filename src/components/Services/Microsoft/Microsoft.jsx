39df9cddc1469931c348f5d51b31218520db09af
Prabhashyam <116556508+7893787849@users.noreply.github.com>
2024-03-06 21:56:52 +0530

    hi

--- src/components/Services/Microsoft/Microsoft.jsx
+++ src/components/Services/Microsoft/Microsoft.jsx
@@ -216,6 +216,30 @@ function Microsoft() {
           </Link>
         </div>
       </div>
+
+      <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+  <div class="relative isolate overflow-hidden bg-blue-800 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
+    <h2 class="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">Experience the power of Microsoft technology with Spyd Technology by your side. Get Started Today!</h2>
+    <p class="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-200">Contact us today to learn more about how our solutions can elevate your business and propel you towards success in the digital age.</p>
+    <div class="mt-10 flex items-center justify-center gap-x-6">
+      <a class="rounded-md bg-green-500 px-5 py-3 text-lg font-semibold text-white shadow-md hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+        href="/signup">Get Started Now</a>
+    </div>
+    <svg viewBox="0 0 1024 1024" class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2"
+      aria-hidden="true">
+      <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.7"></circle>
+      <defs>
+        <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(512 512) rotate(90) scale(512)">
+          <stop stop-color="#7775D6"></stop>
+          <stop offset="1" stop-color="#E935C1" stop-opacity="0"></stop>
+        </radialGradient>
+      </defs>
+    </svg>
+  </div>
+</div>
+
+
       <Approch />
     </>
   )
