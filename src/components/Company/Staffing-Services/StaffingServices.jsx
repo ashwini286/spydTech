bb8cc9037b23b7241f5deade4423114151caa6c6
Ashwini <ashwini.swe@gmail.com>
2024-03-16 20:21:13 +0530

    making StaffingServices page

--- src/components/Company/Staffing-Services/StaffingServices.jsx
+++ src/components/Company/Staffing-Services/StaffingServices.jsx
@@ -12,6 +12,9 @@ const StaffingServices = () => {
         alt="Background Image"
       />
  <div className="absolute inset-0 bg-gradient-to-t from-transparent to-black opacity-70"></div>
+ <div className='flex items-center justify-center' data-aos="fade-up-right">
+        <div className='text-center text-2xl md:pt-16  md:mt-8 font-bold'>Unlocking Excellence:<span className="text-[#005f73] text-4xl mx-auto border-b-2 border-[#005f73] ">  Our Comprehensive Staffing Solutions </span> </div>
+      </div>
     </>
   )
 }
