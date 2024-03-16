3e5aa62ef2bb74f9614aa0cfcf4ca66e22728a9a
Ashwini <ashwini.swe@gmail.com>
2024-03-16 17:41:16 +0530

    ReactJs page edit

--- src/components/Services/Reactjs/ReactImmenseBenefits/Benefits.jsx
+++ src/components/Services/Reactjs/ReactImmenseBenefits/Benefits.jsx
@@ -9,32 +9,38 @@ const Benefits = () => {
   });
   return (
     <>
-      <div className='mt-4 md:px-20 pb-16'  data-aos="fade-up">
+      <div className='mt-4 md:px-20 py-8 bg-gray-300 mb-8'  data-aos="fade-up">
         <div className='md:text-5xl md:font-extrabold text-2xl font-bold pl-12 pb-4'>
           The immense benefits of ReactJS
         </div>
-        <div className='grid grid-cols-1 md:grid-cols-2 md:px-8  text-black'>
-          <div className='flex items-center w-[89%] gap-4 pb-8 pl-2'>
-            <p><TaskAltOutlinedIcon color="primary" /></p>
-            <p>ReactJS provides a powerful library for building web applications with precision and accuracy.</p>
-          </div>
-          <div className='flex items-center w-[89%] gap-4 pb-8 pl-2'>
-            <p><TaskAltOutlinedIcon color="primary" /></p>
-            <p>Efficiency is significantly improved with ReactJS as it requires minimal coding, benefiting both developers and clients.</p>
-          </div>
-          <div className='flex items-center w-[89%] gap-4 pb-8 pl-2'>
-            <p><TaskAltOutlinedIcon color="primary" /></p>
-            <p>The component-based architecture of React makes it easily adaptable to the web applications being developed.</p>
-          </div>
-          <div className='flex items-center w-[89%] gap-4 pb-8 pl-2'>
-            <p><TaskAltOutlinedIcon color="primary" /></p>
-            <p>Backed by Facebook and a large community, React receives regular updates and introduces new features with minimal effort for integration.</p>
-          </div>
-          <div className='flex items-center w-[89%] gap-4 pl-2'>
-            <p><TaskAltOutlinedIcon color="primary" /></p>
-            <p>React provides a rich and seamless user experience, making web applications more responsive and enhancing user satisfaction.</p>
-          </div>
-        </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 md:px-8 text-black'>
+  <div className='flex items-center w-[89%] gap-4 pb-8 pl-2'>
+    <p><TaskAltOutlinedIcon color="primary" /></p>
+    <p>Powerful: ReactJS offers a robust library for precise web application development.</p>
+  </div>
+  <div className='flex items-center w-[89%] gap-4 pb-8 pl-2'>
+    <p><TaskAltOutlinedIcon color="primary" /></p>
+    <p>Efficiency: With minimal coding requirements, ReactJS enhances productivity for both developers and clients.</p>
+  </div>
+  <div className='flex items-center w-[89%] gap-4 pb-8 pl-2'>
+    <p><TaskAltOutlinedIcon color="primary" /></p>
+    <p>Versatility: ReactJS can be used to build various types of applications, ranging from simple single-page apps to complex enterprise-level solutions.</p>
+  </div>
+ 
+  <div className='flex items-center w-[89%] gap-4 pb-8 pl-2'>
+    <p><TaskAltOutlinedIcon color="primary" /></p>
+    <p>Continuous Improvement: Supported by Facebook and a vibrant community, React receives regular updates and introduces new features seamlessly.</p>
+  </div>
+  <div className='flex items-center w-[89%] gap-4 pl-2'>
+    <p><TaskAltOutlinedIcon color="primary" /></p>
+    <p>Enhanced User Experience: React delivers a smooth and responsive user interface, elevating user satisfaction.</p>
+  </div>
+  <div className='flex items-center w-[89%] gap-4 pb-8 pl-2'>
+    <p><TaskAltOutlinedIcon color="primary" /></p>
+    <p>Adaptability: React's component-based architecture makes it easy to adapt to evolving web applications.</p>
+  </div>
+</div>
+
       </div>
     </>
   )
--- src/components/Services/Reactjs/Reactjs.jsx
+++ src/components/Services/Reactjs/Reactjs.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Navbarupp from "../../Home/Navbar/Navbarupp";
 import Development from '../Reactjs/ReactDevelopmentCompany/Development';
 import Benefits from './ReactImmenseBenefits/Benefits';
@@ -7,6 +7,9 @@ import Benefits from './ReactImmenseBenefits/Benefits';
 import Approch from '../../Home/APProchUs/Approch';
 
 const Reactjs = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <>
@@ -17,10 +20,13 @@ const Reactjs = () => {
 
 
         <img
-          src="https://www.paragyte.com/img/React_Banner.png"
-          className=" w-full md:h-[500px]"
+          src="https://www.getaprogrammer.com.au/wp-content/uploads/2020/04/react.jpg"
+          className=" w-full"
           alt="Background Image"
         />
+        <div className='flex items-center justify-center pt-8'>
+        <div className='text-center md:text-2xl text-xl md:pt-16 w-[850px]  font-bold'>Building Dynamic and Responsive Web Applications: <br /> <span className="text-blue-600 md:text-4xl text-2xl mx-auto border-b-2 border-cyan-800 w-1/2"> Mastering ReactJS </span> </div>
+      </div>
         <Development />
         <Benefits />
        
