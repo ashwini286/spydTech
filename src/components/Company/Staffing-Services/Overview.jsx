db51eb279d228d6caa5ed8487f246629f99808f5
Ashwini <ashwini.swe@gmail.com>
2024-03-20 18:23:25 +0530

    set counting Animation in staffing page

--- src/components/Company/Staffing-Services/Overview.jsx
+++ src/components/Company/Staffing-Services/Overview.jsx
@@ -1,43 +1,64 @@
-import React from 'react'
-
+import React from "react";
+import CountingAnimation from "./CountingAnimation";
 function Overview() {
   return (
     <>
-    <div class="bg-white dark:bg-gray-900 h-screen">
-    <div class="container mx-auto py-16 dark:text-white">
-        <div class="text-center">
-            <h2 class="font-bold text-2xl md:text-4xl my-4">Solutions That Funnel Results</h2>
+      <div class="bg-white dark:bg-gray-900 h-screen">
+        <div class="container mx-auto py-16 dark:text-white">
+          <div class="text-center">
+            <h2 class="font-bold text-2xl md:text-4xl my-4">
+              Solutions That Funnel Results
+            </h2>
             <div class="max-w-2xl md:max-w-4xl mx-auto dark:text-gray-300">
-                <p>
-                Driving results depend on a number of factors over and above organic or paid traffic.
-                 Effective lead capture techniques, 
-                 data nurturing & intelligent alert technology all drive higher percentages of conversion and results.
-                 
-                  </p>
+              <p>
+                Driving results depend on a number of factors over and above
+                organic or paid traffic. Effective lead capture techniques, data
+                nurturing & intelligent alert technology all drive higher
+                percentages of conversion and results.
+              </p>
             </div>
+          </div>
+          <div class="grid grid-cols-1 md:grid-cols-2 mx-auto max-w-xl md:max-w-2xl gap-4 my-4">
+            <div class="p-4 border flex flex-col items-center rounded max-w-sm dark:bg-gray-800">
+              <div class="text-4xl font-bold flex justify-center items-center flex-row gap-x-2 ">
+                <div>
+                  <CountingAnimation start={0} end={150} duration={3000} />
+                </div>
+                <div class="text-yellow-500">+</div>
+              </div>
+              <div class="text-300 text-uppercase">Features Analyzed</div>
+            </div>
+            <div class="p-4 border flex flex-col items-center rounded max-w-sm dark:bg-gray-800">
+              <div class="text-4xl font-bold flex justify-center items-center flex-row gap-x-2 ">
+                <CountingAnimation start={0} end={60} duration={3000} />
+                <span class="text-teal-500">+</span>
+              </div>
+              <div class="text-300 text-uppercase">Modules</div>
+            </div>
+            <div class="p-4 border flex flex-col items-center rounded max-w-sm dark:bg-gray-800">
+              <div class="text-4xl font-bold flex justify-center items-center flex-row gap-x-2 ">
+                <div>
+                  <CountingAnimation start={0} end={5} duration={3000} />
+                </div>
+                <div class="text-pink-500">+</div>
+              </div>
+              <div class="text-300 text-uppercase">Functionalities</div>
+            </div>
+            <div class="p-4 border flex flex-col items-center rounded max-w-sm dark:bg-gray-800">
+              <div class="text-4xl font-bold flex justify-center items-center flex-row gap-x-2">
+                <div>
+                  {" "}
+                  <CountingAnimation start={0} end={4} duration={1000} />
+                </div>
+                <div class="text-violet-500">+</div>
+              </div>
+              <div class="text-300 text-uppercase">File Formats Supported</div>
+            </div>
+          </div>
         </div>
-        <div class="grid grid-cols-2 mx-auto max-w-xl md:max-w-2xl gap-4 my-4">
-            <div class="p-4 border flex flex-col items-center rounded max-w-sm dark:bg-gray-800">
-                <div class="text-4xl font-bold">150<span class="text-yellow-500">+</span></div>
-                <div class="text-300 text-uppercase">Features Analyzed</div>
-            </div>
-            <div class="p-4 border flex flex-col items-center rounded max-w-sm dark:bg-gray-800">
-                <div class="text-4xl font-bold">60<span class="text-teal-500">+</span></div>
-                <div class="text-300 text-uppercase">Modules</div>
-            </div>
-            <div class="p-4 border flex flex-col items-center rounded max-w-sm dark:bg-gray-800">
-                <div class="text-4xl font-bold">5<span class="text-pink-500">+</span></div>
-                <div class="text-300 text-uppercase">Functionalities</div>
-            </div>
-            <div class="p-4 border flex flex-col items-center rounded max-w-sm dark:bg-gray-800">
-                <div class="text-4xl font-bold">4<span class="text-violet-500">+</span></div>
-                <div class="text-300 text-uppercase">File Formats Supported</div>
-            </div>
-        </div>
-    </div>
-</div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Overview
+export default Overview;
